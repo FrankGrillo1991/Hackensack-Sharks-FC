00d7182cf9579d9a26fb82cd60362032a0fa8138
FrankGrillo1991 <fsg12491@aol.com>
2025-07-28 21:34:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -159,3 +159,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 10000);
     }
 });
+
+function openLightbox(imgElement) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = imgElement.src;
+    lightbox.style.display = 'block';
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
